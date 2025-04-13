5a473dd852c88dcb8399ff4a53b6061db955b8c8
Praneetha Reddy <praneethapuchalapalli20@gmail.com>
2025-04-13 20:45:22 +0530

    Episode-2 Igniting our App

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ const element = React.createElement(
         {id: 'child1'},
         [React.createElement('h1', null, 'H1 tag'),
         React.createElement('h2', null, 'H2 tag')]
-    ), React.createElement(
-        'div',
-        {id: 'child2'},
-        [React.createElement('h1', null, 'H1 tag'),
-        React.createElement('h2', null, 'H2 tag')]
     )]
 );
+//JSX - It is syntactic sugar for React.createElement
+//JS engine does not understand JSX, it needs to be converted to JS using Babel.
+//JSX is transpiled to React.createElement calls by Babel.
+const heading = <h1>Namaste React using JSX</h1>
 const rootReact = ReactDOM.createRoot(document.getElementById('root'));
-rootReact.render(element);
+rootReact.render(heading);
